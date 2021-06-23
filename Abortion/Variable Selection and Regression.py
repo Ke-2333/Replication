779c2dc74a07a32a71e0e622ec3e2e999007c4b1
Kexuan Song <71377582+Ke-2333@users.noreply.github.com>
2021-06-23 13:52:52 +0800

    Add files via upload

--- Abortion/Variable Selection and Regression.py
+++ Abortion/Variable Selection and Regression.py
@@ -8,8 +8,8 @@ Created on Wed Jun  9 16:57:17 2021
 
 import numpy as np
 import pandas as pd
-import os
-os.chdir("F:\\!BENTLEY\\RA\\Replication")
+#import os
+#os.chdir("F:\\!BENTLEY\\RA\\Replication")
 import statsmodels.api as sm
 
 
@@ -21,7 +21,7 @@ cols = df.columns
 
 #controls
 tdums = df.loc[:,'_Iyear_87':'_Iyear_97'] #(600,11)
-tdums_con = np.array(tdums)
+tdumsCtrl = np.array(tdums)
 # =============================================================================
 # ['_Iyear_87', '_Iyear_88', '_Iyear_89', '_Iyear_90', '_Iyear_91',
 #  '_Iyear_92', '_Iyear_93', '_Iyear_94', '_Iyear_95', '_Iyear_96', '_Iyear_97']
@@ -75,13 +75,13 @@ import LassoShooting as la
 
 #Violence Outcome--------------------------------------------------------------
 #first selection
-violBeta1, violIndex1 = la.LassoShooting(Dyviol, AllViol_X, controls = tdums_con)
+violBeta1, violIndex1 = la.LassoShooting(Dyviol, AllViol_X, controls = tdumsCtrl)
 AllViol.columns[violIndex1]
 #[]
 #stata: null
 
 #second selection
-violBeta2, violIndex2 = la.LassoShooting(Dviol, AllViol_X, controls = tdums_con)
+violBeta2, violIndex2 = la.LassoShooting(Dviol, AllViol_X, controls = tdumsCtrl)
 AllViol.columns[violIndex2]
 #['viol0', 'Lxxprison', 'Lxxpolice', 'Mxxincome', 'Dxxincome0','LxxpoliceXt', 'MxxincomeXt', 'Dxxincome0Xt', 'Dxxbeer0Xt'],
 #stata :viol0 Lxxprison Lxxpolice Mxxincome Dxxincome0 LxxpoliceXt MxxincomeXt Dxxincome0Xt Dxxbeer0Xt
@@ -97,15 +97,16 @@ X_viol = sm.add_constant(X_viol)
 violResult = sm.OLS(df['Dyviol'],X_viol).fit(cov_type='cluster',
                                              cov_kwds={'groups':df['statenum']})
 violResult.summary()
+violResult.mse_resid
 
 
 #Property Outcome--------------------------------------------------------------
-propBeta1, propIndex1 = la.LassoShooting(Dyprop, AllProp_X, controls = tdums_con)
-propSel1 = AllProp.columns[propIndex1]
+propBeta1, propIndex1 = la.LassoShooting(Dyprop, AllProp_X, controls = tdumsCtrl)
+AllProp.columns[propIndex1]
 #['Mxxincome2Xt2', 'xxincome02Xt2']
 #stata:Mxxincome2Xt2 xxincome02Xt2
 
-propBeta2, propIndex2 = la.LassoShooting(Dprop, AllProp_X, controls = tdums_con)
+propBeta2, propIndex2 = la.LassoShooting(Dprop, AllProp_X, controls = tdumsCtrl)
 AllProp.columns[propIndex2]
 #['prop0', 'Lxxprison', 'Lxxpolice', 'Lxxincome', 'Mxxincome', 'Dxxincome0', 'Dxxincome0Xt']
 #stata:prop0 Lxxprison Lxxpolice Lxxincome Mxxincome Dxxincome0 Dxxincome0Xt
@@ -119,15 +120,16 @@ X_prop = sm.add_constant(X_prop)
 propResult = sm.OLS(df['Dyprop'],X_prop).fit(cov_type='cluster',
                                              cov_kwds={'groups':df['statenum']})
 propResult.summary()
+propResult.mse_resid
 
 
 #Muder Outcome----------------------------------------------------------------
-murdBeta1, murdIndex1 = la.LassoShooting(Dymurd, AllMurd_X, controls = tdums_con)
+murdBeta1, murdIndex1 = la.LassoShooting(Dymurd, AllMurd_X, controls = tdumsCtrl)
 AllMurd.columns[murdIndex1]
 #[]
 #stata: null
 
-murdBeta2, murdIndex2 = la.LassoShooting(Dmurd, AllMurd_X, controls = tdums_con)
+murdBeta2, murdIndex2 = la.LassoShooting(Dmurd, AllMurd_X, controls = tdumsCtrl)
 AllMurd.columns[murdIndex2]
 #['murd0', 'murd0Xt', 'Lxxprison', 'LxxprisonXt', 'LxxpoliceXt','MxxincomeXt', 'Dxxincome0Xt']
 #stata:murd0 murd0Xt Lxxprison LxxprisonXt LxxpoliceXt MxxincomeXt Dxxincome0Xt
@@ -141,13 +143,176 @@ X_murd = sm.add_constant(X_murd)
 murdResult = sm.OLS(df['Dymurd'],X_murd).fit(cov_type='cluster',
                                              cov_kwds={'groups':df['statenum']})
 murdResult.summary()
-
-
-1. partial out controls
-2. y multivariable nx2   
-ft twice / once 
-
-sir twice
+murdResult.mse_resid
 
 
 
+#using 'ft' and 'sir'
+from FTIRE import ftire
+#partial out controls
+Dyviol_re = la.partial_out(Dyviol, tdumsCtrl)
+Dviol_re = la.partial_out(Dviol, tdumsCtrl)
+AllViol_X_re = la.partial_out(AllViol_X, tdumsCtrl)
+
+Dyprop_re = la.partial_out(Dyprop, tdumsCtrl)
+Dprop_re = la.partial_out(Dprop, tdumsCtrl)
+AllProp_X_re = la.partial_out(AllProp_X, tdumsCtrl)
+
+Dymurd_re = la.partial_out(Dymurd, tdumsCtrl)
+Dmurd_re = la.partial_out(Dmurd, tdumsCtrl)
+AllMurd_X_re = la.partial_out(AllMurd_X, tdumsCtrl)
+
+# Using 'ft' twice-------------------------------------------------------------
+# Violence outcome-------------------------------------------------------------
+#'ft' first step
+B_viol_ft1,_,_,_ = ftire.CV(AllViol_X_re,Dyviol_re,1,30,'ft')
+viol_ft1 = np.dot(AllViol_X_re, B_viol_ft1)
+
+#'ft' second step
+B_vio_ft2,_,_,_ = ftire.CV(AllViol_X_re,Dviol_re,1,30,'ft')
+viol_ft2 = np.dot(AllViol_X_re, B_viol_ft2)
+
+X_viol_ft_reduced = pd.DataFrame(np.hstack([viol_ft1,viol_ft2]), columns = ['ft_viol1','ft_viol2'])
+
+#regression
+X_viol_ft = pd.concat([df['Dviol'],X_viol_ft_reduced,tdums], axis = 1) 
+X_viol_ft = sm.add_constant(X_viol_ft)
+viol_ft_reg = sm.OLS(df['Dyprop'],X_viol_ft).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+viol_ft_reg.summary()
+viol_ft_reg.mse_resid
+
+# Property outcome-------------------------------------------------------------
+#'ft' first step
+B_prop_ft1,_,_,_ = ftire.CV(AllProp_X_re,Dyprop_re,1,30,'ft')
+prop_ft1 = np.dot(AllProp_X_re, B_prop_ft1)
+
+#'ft' second step
+B_prop_ft2,_,_,_ = ftire.CV(AllProp_X_re,Dprop_re,1,30,'ft')
+prop_ft2 = np.dot(AllProp_X_re, B_ft2)
+
+X_prop_ft_reduced = pd.DataFrame(np.hstack([prop_ft1,prop_ft2]), columns = ['ft_prop1','ft_prop2'])
+
+#regression
+X_prop_ft = pd.concat([df['Dprop'],X_prop_ft_reduced,tdums], axis = 1) 
+X_prop_ft = sm.add_constant(X_prop_ft)
+prop_ft_reg = sm.OLS(df['Dyprop'],X_prop_ft).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+prop_ft_reg.summary()
+prop_ft_reg.mse_resid
+
+# Murder outcome---------------------------------------------------------------
+B_murd_ft1,_,_,_ = ftire.CV(AllMurd_X_re,Dymurd_re,1,30,'ft')
+murd_ft1 = np.dot(AllMurd_X_re, B_murd_ft1)
+
+#'ft' second step
+B_murd_ft2,_,_,_ = ftire.CV(AllMurd_X_re,Dmurd_re,1,30,'ft')
+murd_ft2 = np.dot(AllMurd_X_re, B_ft2)
+
+X_murd_ft_reduced = pd.DataFrame(np.hstack([murd_ft1,murd_ft2]), columns = ['ft_murd1','ft_murd2'])
+
+#regression
+X_murd_ft = pd.concat([df['Dmurd'],X_murd_ft_reduced,tdums], axis = 1) 
+X_murd_ft = sm.add_constant(X_murd_ft)
+murd_ft_reg = sm.OLS(df['Dymurd'],X_murd_ft).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+murd_ft_reg.summary()
+murd_ft_reg.mse_resid
+
+
+#Using 'ft' once---------------------------------------------------------------
+# Violence Outcome-------------------------------------------------------------
+B_viol_ft_V2,_,_,_ = ftire.CV(AllViol_X_re,np.hstack([Dyviol_re,Dviol_re]),1,30,'ft')
+viol_ft_V2 = np.dot(AllViol_X_re, B_viol_ft_V2)
+X_viol_ft_reducedV2 = pd.DataFrame(viol_ft_V2, columns = ['ft_viol_V2'])
+
+X_viol_ft_V2 = pd.concat([df['Dviol'],X_viol_ft_reducedV2,tdums], axis = 1) 
+X_viol_ft_V2 = sm.add_constant(X_viol_ft_V2)
+viol_ft_reg_V2 = sm.OLS(df['Dyprop'],X_viol_ft_V2).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+viol_ft_reg_V2.summary()
+viol_ft_reg_V2.mse_resid
+
+# Property Outcome-------------------------------------------------------------
+B_prop_ft_V2,_,_,_ = ftire.CV(AllProp_X_re,np.hstack([Dyprop_re,Dprop_re]),1,30,'ft')
+prop_ft_V2 = np.dot(AllProp_X_re, B_prop_ft_V2)
+X_prop_ft_reducedV2 = pd.DataFrame(prop_ft_V2, columns = ['ft_prop_V2'])
+
+X_prop_ft_V2 = pd.concat([df['Dprop'],X_prop_ft_reducedV2,tdums], axis = 1) 
+X_prop_ft_V2 = sm.add_constant(X_prop_ft_V2)
+prop_ft_reg_V2 = sm.OLS(df['Dyprop'],X_prop_ft_V2).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+prop_ft_reg_V2.summary()
+prop_ft_reg_V2.mse_resid
+
+# Murder Outcome---------------------------------------------------------------
+B_murd_ft_V2,_,_,_ = ftire.CV(AllMurd_X_re,np.hstack([Dymurd_re,Dmurd_re]),1,30,'ft')
+murd_ft_V2 = np.dot(AllMurd_X_re, B_murd_ft_V2)
+X_murd_ft_reducedV2 = pd.DataFrame(murd_ft_V2, columns = ['ft_murd_V2'])
+
+X_murd_ft_V2 = pd.concat([df['Dmurd'],X_murd_ft_reducedV2,tdums], axis = 1) 
+X_murd_ft_V2 = sm.add_constant(X_murd_ft_V2)
+murd_ft_reg_V2 = sm.OLS(df['Dymurd'],X_murd_ft_V2).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+murd_ft_reg_V2.summary()
+murd_ft_reg_V2.mse_resid
+
+
+#Using 'sir'-------------------------------------------------------------------
+# Violence outcome-------------------------------------------------------------
+#'sir' first step
+B_viol_sir1,_,_,_ = ftire.CV(AllViol_X_re,Dyviol_re,1,5,'sir')
+viol_sir1 = np.dot(AllViol_X_re, B_viol_sir1)
+
+#'sir' second step
+B_viol_sir2,_,_,_ = ftire.CV(AllViol_X_re,Dviol_re,1,5,'sir')
+viol_sir2 = np.dot(AllViol_X_re, B_viol_sir2)
+
+X_viol_sir_reduced = pd.DataFrame(np.hstack([viol_sir1,viol_sir2]), columns = ['sir_viol1','sir_viol2'])
+
+#regression
+X_viol_sir = pd.concat([df['Dviol'],X_viol_sir_reduced,tdums], axis = 1) 
+X_viol_sir = sm.add_constant(X_viol_sir)
+viol_sir_reg = sm.OLS(df['Dyprop'],X_viol_sir).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+viol_sir_reg.summary()
+viol_sir_reg.mse_resid
+
+# Property outcome-------------------------------------------------------------
+#'sir' first step
+B_prop_sir1,_,_,_ = ftire.CV(AllProp_X_re,Dyprop_re,1,5,'sir')
+prop_sir1 = np.dot(AllProp_X_re, B_prop_sir1)
+
+#'sir' second step
+B_prop_sir2,_,_,_ = ftire.CV(AllProp_X_re,Dprop_re,1,5,'sir')
+prop_sir2 = np.dot(AllProp_X_re, B_prop_sir2)
+
+X_prop_sir_reduced = pd.DataFrame(np.hstack([prop_sir1,prop_sir2]), columns = ['sir_prop1','sir_prop2'])
+
+#regression
+X_prop_sir = pd.concat([df['Dprop'],X_prop_sir_reduced,tdums], axis = 1) 
+X_prop_sir = sm.add_constant(X_prop_sir)
+prop_sir_reg = sm.OLS(df['Dyprop'],X_prop_sir).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+prop_sir_reg.summary()
+prop_sir_reg.mse_resid
+
+
+# Murder outcome---------------------------------------------------------------
+#'sir' first step
+B_murd_sir1,_,_,_ = ftire.CV(AllMurd_X_re,Dymurd_re,1,5,'sir')
+murd_sir1 = np.dot(AllMurd_X_re, B_murd_sir1)
+
+#'sir' second step
+B_murd_sir2,_,_,_ = ftire.CV(AllMurd_X_re,Dmurd_re,1,5,'sir')
+murd_sir2 = np.dot(AllMurd_X_re, B_murd_sir2)
+
+X_murd_sir_reduced = pd.DataFrame(np.hstack([murd_sir1,murd_sir2]), columns = ['sir_murd1','sir_murd2'])
+
+#regression
+X_murd_sir = pd.concat([df['Dmurd'],X_murd_sir_reduced,tdums], axis = 1) 
+X_murd_sir = sm.add_constant(X_murd_sir)
+murd_sir_reg = sm.OLS(df['Dymurd'],X_murd_sir).fit(cov_type='cluster',
+                                             cov_kwds={'groups':df['statenum']})
+murd_sir_reg.summary()
+murd_sir_reg.mse_resid
